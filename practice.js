b6f1968208b0a57738b6319a6fcf3cd7f05ef9b7
shamroz khan <124503790+youngs01@users.noreply.github.com>
2024-01-10 03:05:56 -0800

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,79 @@
+// let fullname = prompt("Enter your full name");
+// if(fullname.includes(" ")){
+// alert("Don't include spaces in your name")
+// }else{
+//     let userLength = fullname.length
+//     let username = "@" + fullname + userLength; 
+//     console.log(username)    
+// }
+
+// function pushArray(num){
+//     let myArr = [1,2,3,4,5]
+//      newArr = myArr + "," + num; 
+// }
+// pushArray(10,20,30);
+// console.log(newArr)
+
+
+// create a function that take a string from user and return vowels in the string to the user
+
+// function userString(str) {
+//     var count = 0;
+//     for (const char of str) {
+//         if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+//          count++;
+//         }
+//     }
+//     console.log(count)
+// }
+
+// userString("shamroz");
+
+const userString == (str) =>{
+    let count = 0;
+    for (let char of str) {
+        if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
+         count++;
+        }
+    }
+    console.log(count)
+}
+userString("shamroz")
+
+
+
+// const userString = (str) => {
+//     let count = 0;
+//     for (let char of str) {
+//         if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+//             count++;
+//         }
+//     }
+//     console.log(count);
+// }
+
+// userString("shamroz");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
